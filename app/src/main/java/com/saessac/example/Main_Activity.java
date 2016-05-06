91f85930166f768b9c80aeda08ee25656f44ef2c
likealike <vision51@gmail.com>
2016-05-06 10:14:55 +0900

    initialize project

---
+++ app/src/main/java/com/saessac/example/Main_Activity.java
@@ -0,0 +1,13 @@
+package com.saessac.example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
